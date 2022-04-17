f2c52586441aaf824dc995fd853649457aaa899a
lutfiqaraman <lutfiqaraman@gmail.com>
2022-04-18 00:00:48 +0300

    Add classes

---
+++ Source.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleAutoMapper
+{
+    public class Source
+    {
+        public int Value { get; set; }
+    }
+}
